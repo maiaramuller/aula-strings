c440a4a53dd607c59f0933d5ceebac269b7515e2
maiara <maiara.15muller@outlook.com>
2023-08-22 21:41:34 -0300

    desafio

---
+++ desafio.js
@@ -0,0 +1,12 @@
+/*1.let frase = prompt("Escreva uma frase:");
+let resultado = frase.split(" ");
+console.log(resultado);*/
+
+/*2;
+let array = ["Banana", "Morango", "Abacaxi", "Laranja", "Ameixa"];
+const tamanho = array.length;
+const resultado = Number(tamanho) - 3;
+const indiceAbacaxi = resultado;
+console.log("Indice do Abacaxi", indiceAbacaxi);
+console.log("Tamanhio do Array", tamanho);
+*/
