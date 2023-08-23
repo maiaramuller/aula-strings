dfde30924dc236708ddc19c51f792849b3fbf7de
maiara <maiara.15muller@outlook.com>
2023-08-22 21:19:31 -0300

    Exercicios aula 10

---
+++ index.js
@@ -0,0 +1,66 @@
+/*
+1.
+let array
+console.log('a. undefaine', array)
+
+array = null
+console.log('b. null ', array)
+
+array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+console.log('c. 11', array.length)
+
+let i = 0
+console.log('d. 3', array[i])
+
+array[i+1] = 19
+console.log('e. [3, 19, 5, 6, 7, 8, 9, 10, 11, 12, 13]', array)
+
+const valor = array[i+6]
+console.log('f. 9', valor)*/
+
+/*
+2.
+const frase = prompt("Digite uma frase")
+
+console.log(frase.toUpperCase().replaceAll("A", "I"), frase.length)
+
+console.log (SUBI NUM ÔNIBUS EM MIRROCOS 27)*/
+
+//Exercícios de escrita
+
+/*
+1.
+const email = prompt("Qual é o seu email?");
+const nome = prompt("Qul é seu nome?");
+
+console.log(
+  "O e-mail " + email + " foi cadastrado com sucesso. Seja bem-vinda(o), " + nome + "!");*/
+
+/*2;
+
+let comidas = ["pizza", "açai", "chocolate", "lasanha", "sorvete"];
+
+//a. console.log(comidas);
+
+//b. console.log("Essas são as minhas comidas preferidas:", ["pizza", "açai", "chocolate", "lasanha", "sorvete"]);
+
+
+c. let favorita = prompt("Qual é a sua comida favorita?");
+
+comidas[1] = favorita;
+console.log(comidas);*/
+
+/*3;
+let listaDeTarefas = [];
+let tarefas1 = prompt("Digite a primeira tarefas que você realiza no dia:");
+let tarefas2 = prompt("Digite a segunda tarefas que você realiza no dia:");
+let tarefas3 = prompt("Digite a terceira tarefas que você realiza no dia:");
+
+listaDeTarefas.push(tarefas1);
+listaDeTarefas.push(tarefas2);
+listaDeTarefas.push(tarefas3);
+console.log(listaDeTarefas);
+
+let pergunta = prompt("Digite o indice de uma tarefa que você já realizou:");
+listaDeTarefas.splice(pergunta, 1);
+console.log(listaDeTarefas);*/
